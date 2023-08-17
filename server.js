facfa1bbe265abd6f7045003425525a442e5355d
Chris <martin.christohercharles@gmail.com>
2023-08-17 14:44:36 -0700

    server setup for real-time chat app

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+const { log } = require('console');
 const express = require('express');
 const { createServer } = require('http');
+const { type } = require('os');
 const { Server } = require('socket.io');
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.route("/").get((req, res) => {
-    res.json("Hello world! You're a little ho bitch fucker!")
-})
+// app.route("/").get((req, res) => {
+//     res.json("My little server")
+// })
 
 io.on("connection", (socket) => {
-    // ...
+    socket.join("chat_group")
+    console.log("User connected!");
+    socket.on('sendMessage', (data) => {
+        console.log("Data received: ", data);
+        // socket.emit("serverMessage", {...data, type:"serverMessage"});
+        io.to("chat_group").emit("serverMessage", {...data, type:"serverMessage"});
+    })
 });
 
 httpServer.listen(3000);
